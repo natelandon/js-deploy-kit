b153d6ad0621c110c4e35f8d94a7c97859d33a01
Chad Martin <Chad Martin>
2017-05-01 15:24:07 -0700

    azure 25

--- src/server/core/baseUrl.js
+++ src/server/core/baseUrl.js
@@ -1,7 +1,36 @@
 export default function getBaseUrl() {
-  return getQueryStringParameterByName('usedevApi') ? `http://localhost:3000` : `http://codercamps-mic.azurewebsites.net`;
+  return getApiurl();
 }
 
+let testLocalhost= (string)=>
+{
+
+  let host = "localhost";	// Variable Name 1
+
+      let mHost = string.match(host);
+      return (mHost !== null ) ?  true : false;
+
+}
+let testHTPPS= (string)=>
+{
+  let https ="https:";	// Variable Name 1
+  let m = string.match(https);
+  return (m.length > 0 ) ? true : false
+}
+function getApiurl() {
+  let url = window.location.href;
+  if (testLocalhost(url))
+  {
+    return  `http://localhost:3000`;
+  }
+  else return (testHTPPS(url) !== null ) ?   `https://codercamps-mic.azurewebsites.net` : `http://codercamps-mic.azurewebsites.net`;
+
+}
+
+
+
+
+/**
 function getQueryStringParameterByName(name, url) {
   if (!url) url = window.location.href;
   name = name.replace(/[\[\]]/g, "\\$&");
@@ -11,3 +40,4 @@ function getQueryStringParameterByName(name, url) {
   if (!results[2]) return '';
   return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
+**/
